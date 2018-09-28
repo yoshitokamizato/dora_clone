2acd317690d1cd4144e47710a16a189ecc75b416
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-09-28 14:52:13 +0900

    modify main.rb

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@ require './ynakamura'
 
 kamizato = Kamizato.new("kamizato", 30, 20)
 ynakamura = Ynakamura.new
+tkase = Tkase.new
 
 puts kamizato.disp_params
 puts "私の名前は#{ynakamura.name}です。"
+puts "私は、#{{tkase.name}です}"
