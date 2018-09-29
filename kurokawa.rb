f807556c97590e9a3383192c59e877ddeb5103b6
yuri kurokawa <mp963@ezweb.ne.jp>
2018-09-29 21:57:53 +0900

    add kurokawa.rb,main.rb

---
+++ kurokawa.rb
@@ -0,0 +1,6 @@
+class Kurokawa
+  attr_accessor :name
+  def initialize(name,hp,mp)
+    self.name = "kurokawa"
+  end
+end
--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ require './ynakamura'
 kamizato = Kamizato.new("kamizato", 30, 20)
 ynakamura = Ynakamura.new
 tkase = Tkase.new
+kurokawa = Kurokawa.new
 
 puts kamizato.disp_params
 puts "私の名前は#{ynakamura.name}です。"
 puts "私は、#{{tkase.name}です}"
+puts "私は#{kurokawa.name}です"
