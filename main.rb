f5de6d0a8a4a77ddaba2bc485e0dd9d69bd741ae
USER <user@USER-no-MacBook-Air-2.local>
2018-10-15 21:56:53 +0900

    revision1

--- main.rb
+++ main.rb
@@ -2,12 +2,15 @@
 require './kamizato'
 require './ynakamura'
 require "./ajk"
-
+require "./tkase"
+require "./kurokawa"
+require "./Satoshi"
 kamizato = Kamizato.new("kamizato", 30, 20)
 ynakamura = Ynakamura.new(name:"なかむら", hp:25, mp:32 ,atk:5, deff:3 )
 tkase = Tkase.new
 kuro = Kuro.new
 ajk = Ajk.new(name: "ajk", hp:28 , mp:15, atk:3, deff:4)
+satoshi = Satoshi.new
 
 puts kamizato.disp_params
 puts ynakamura.disp_params
--- satoshi.rb
+++ satoshi.rb
@@ -1,6 +1,6 @@
-class satoshi
+class Satoshi
     attr_accessor :name
     def initialize
       self.name = "satoshi"
     end
-  end
+end
